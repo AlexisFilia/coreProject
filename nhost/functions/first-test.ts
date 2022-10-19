2c53aec0bd85d577ec129c073ce54d913f2f40e0
Alexis Filia <filia.alexis@gmail.com>
2022-10-19 19:00:40 +0200

    Log in the lambda

--- nhost/functions/first-test.ts
+++ nhost/functions/first-test.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export default (req: Request, res: Response) => {
   // res.status(200).send(`Req: ${req}!`);
-  console.log(req);
+  console.log("Je reçois des emails");
 };
