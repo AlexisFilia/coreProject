b45df3e630e17801514a85dc524245318e227dd1
Alexis Filia <filia.alexis@gmail.com>
2022-10-20 11:32:56 +0200

    Console.log(body)

--- nhost/functions/first-test.ts
+++ nhost/functions/first-test.ts
@@ -3,6 +3,6 @@ import { Request, Response } from "express";
 export default (req: Request, res: Response) => {
   // res.status(200).send(`Req: ${req}!`);
   console.log("Je reçois des emails");
-  console.log(req);
+  console.log(req.body);
   console.log("--------------------");
 };
